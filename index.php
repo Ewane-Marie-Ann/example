e2e860a187abefd9a45aa3e4e77471da3ef4ab23
Marie <ewanemarieann@gmail.com>
2024-10-25 12:50:49 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+$name=2;
+  $name+=6;
+//$name--;
+ //use deffine for constants
+ define ('age',['1','love']);
+ echo  $name ;
+ 
+?> 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>my first php work</title>
+</head>
+<body>
+    <h2><?php
+echo ' i dont know '
+
+?></h2>
+</body>
+</html>
